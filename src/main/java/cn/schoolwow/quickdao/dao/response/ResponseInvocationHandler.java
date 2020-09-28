a5212a6c8d1cfb67c3f9a5fe46243335aab3f0a8
sunyue <648823596@qq.com>
2020-09-28 09:56:54 +0800

    发生异常时打印原始SQL语句

--- src/main/java/cn/schoolwow/quickdao/dao/response/ResponseInvocationHandler.java
+++ src/main/java/cn/schoolwow/quickdao/dao/response/ResponseInvocationHandler.java
@@ -1,11 +1,13 @@
 package cn.schoolwow.quickdao.dao.response;
 
 import cn.schoolwow.quickdao.builder.sql.AbstractSQLBuilder;
+import cn.schoolwow.quickdao.exception.SQLRuntimeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class ResponseInvocationHandler implements InvocationHandler {
@@ -22,14 +24,25 @@ public class ResponseInvocationHandler implements InvocationHandler {
         AbstractSQLBuilder abstractSQLBuilder = (AbstractSQLBuilder) abstractResponse.query.dqlsqlBuilder;
         abstractSQLBuilder.connection = abstractResponse.connection;
         long startTime = System.currentTimeMillis();
-        Object result = method.invoke(abstractResponse, args);
-        long endTime = System.currentTimeMillis();
-        if(null!=MDC.get("name")){
-            logger.debug("[{}]行数:{},耗时:{}ms,执行SQL:{}",MDC.get("name"),MDC.get("count"),(endTime-startTime),MDC.get("sql"));
+        try {
+            Object result = method.invoke(abstractResponse, args);
+            long endTime = System.currentTimeMillis();
+            if(null!=MDC.get("name")){
+                logger.debug("[{}]行数:{},耗时:{}ms,执行SQL:{}",MDC.get("name"),MDC.get("count"),(endTime-startTime),MDC.get("sql"));
+            }
+            return result;
+        }catch (InvocationTargetException e){
+            Throwable targetException = e.getTargetException();
+            if(targetException instanceof SQLRuntimeException){
+                if(null!=MDC.get("name")){
+                    logger.warn("[{}]原始SQL:{}",MDC.get("name"),MDC.get("sql"));
+                }
+            }
+            throw targetException;
+        }finally {
+            abstractResponse.query.parameterIndex = 1;
+            abstractResponse.connection.close();
+            MDC.clear();
         }
-        MDC.clear();
-        abstractResponse.query.parameterIndex = 1;
-        abstractResponse.connection.close();
-        return result;
     }
 }
